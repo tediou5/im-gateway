365fddd5ff312f5a26c41f574e8f13e9f18f602b
qians <qiangezaici@outlook.com>
2023-06-02 18:46:36 +0800

    add readme

--- link/src/linker/protocol/token.rs
+++ link/src/linker/protocol/token.rs
@@ -10,5 +10,15 @@ mod test {
     #[test]
     fn parse_token() {
         use base64::{engine::general_purpose, Engine as _};
+
+        let key: [u8; 32] = [
+            142, 41, 224, 201, 169, 225, 121, 200, 110, 106, 120, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+
+        // Verifing JWT
+        let verifier = HS256.verifier_from_bytes(key).unwrap();
+        let (payload, header) = jwt::decode_with_verifier("eyJhbGciOiJIUzI1NiJ9.eyJzYWx0Ijoid1M5aUhkMmUiLCJwaW4iOiIxMDAwMDAxIiwiYXBwSWQiOiJqTFVUeWlMcCJ9.9AzF2fKc1EY90vrF_qeqgKqhDTX3TmWBgXSdnipCX8s", &verifier).unwrap();
+        println!("payload: {:?}", payload);
     }
 }
